a5013910fb943ad8375217c580544d0e8e89818c
amWRit <derasrocks@gmail.com>
2016-07-29 18:14:53 +0545

    add results

---
+++ ASMAS/addResultLowSecForm.Designer.vb
@@ -0,0 +1,494 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class addResultLowSecForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.studentRegLabel = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.engTh = New System.Windows.Forms.TextBox()
+        Me.engPr = New System.Windows.Forms.TextBox()
+        Me.nepPr = New System.Windows.Forms.TextBox()
+        Me.nepTh = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.sciPr = New System.Windows.Forms.TextBox()
+        Me.sciTh = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.mathPr = New System.Windows.Forms.TextBox()
+        Me.mathTh = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.heaPr = New System.Windows.Forms.TextBox()
+        Me.heaTh = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.compPr = New System.Windows.Forms.TextBox()
+        Me.compTh = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.obtPr = New System.Windows.Forms.TextBox()
+        Me.obtTh = New System.Windows.Forms.TextBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.socPr = New System.Windows.Forms.TextBox()
+        Me.socTh = New System.Windows.Forms.TextBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.morPr = New System.Windows.Forms.TextBox()
+        Me.morTh = New System.Windows.Forms.TextBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.saveBtn = New System.Windows.Forms.Button()
+        Me.cancelBtn = New System.Windows.Forms.Button()
+        Me.termCombo = New System.Windows.Forms.ComboBox()
+        Me.termLabel = New System.Windows.Forms.Label()
+        Me.studentName = New System.Windows.Forms.TextBox()
+        Me.studentRegCombo = New System.Windows.Forms.ComboBox()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MS Reference Sans Serif", 12.0!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(98, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Add Result"
+        '
+        'studentRegLabel
+        '
+        Me.studentRegLabel.AutoSize = True
+        Me.studentRegLabel.Location = New System.Drawing.Point(32, 74)
+        Me.studentRegLabel.Name = "studentRegLabel"
+        Me.studentRegLabel.Size = New System.Drawing.Size(67, 13)
+        Me.studentRegLabel.TabIndex = 26
+        Me.studentRegLabel.Text = "Reg Number"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Adobe Heiti Std R", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label2.Location = New System.Drawing.Point(32, 116)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(54, 16)
+        Me.Label2.TabIndex = 27
+        Me.Label2.Text = "Subject"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Adobe Heiti Std R", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label3.Location = New System.Drawing.Point(172, 114)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(50, 16)
+        Me.Label3.TabIndex = 28
+        Me.Label3.Text = "Theory"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Adobe Heiti Std R", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label4.Location = New System.Drawing.Point(326, 114)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(62, 16)
+        Me.Label4.TabIndex = 29
+        Me.Label4.Text = "Practical"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(32, 149)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(41, 13)
+        Me.Label5.TabIndex = 30
+        Me.Label5.Text = "English"
+        '
+        'engTh
+        '
+        Me.engTh.Location = New System.Drawing.Point(175, 146)
+        Me.engTh.Name = "engTh"
+        Me.engTh.Size = New System.Drawing.Size(100, 20)
+        Me.engTh.TabIndex = 31
+        Me.engTh.Text = "0"
+        '
+        'engPr
+        '
+        Me.engPr.Location = New System.Drawing.Point(329, 146)
+        Me.engPr.Name = "engPr"
+        Me.engPr.Size = New System.Drawing.Size(100, 20)
+        Me.engPr.TabIndex = 32
+        Me.engPr.Text = "0"
+        '
+        'nepPr
+        '
+        Me.nepPr.Location = New System.Drawing.Point(329, 176)
+        Me.nepPr.Name = "nepPr"
+        Me.nepPr.Size = New System.Drawing.Size(100, 20)
+        Me.nepPr.TabIndex = 35
+        Me.nepPr.Text = "0"
+        '
+        'nepTh
+        '
+        Me.nepTh.Location = New System.Drawing.Point(175, 176)
+        Me.nepTh.Name = "nepTh"
+        Me.nepTh.Size = New System.Drawing.Size(100, 20)
+        Me.nepTh.TabIndex = 34
+        Me.nepTh.Text = "0"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(32, 179)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(37, 13)
+        Me.Label6.TabIndex = 33
+        Me.Label6.Text = "Nepali"
+        '
+        'sciPr
+        '
+        Me.sciPr.Location = New System.Drawing.Point(329, 238)
+        Me.sciPr.Name = "sciPr"
+        Me.sciPr.Size = New System.Drawing.Size(100, 20)
+        Me.sciPr.TabIndex = 41
+        Me.sciPr.Text = "0"
+        '
+        'sciTh
+        '
+        Me.sciTh.Location = New System.Drawing.Point(175, 238)
+        Me.sciTh.Name = "sciTh"
+        Me.sciTh.Size = New System.Drawing.Size(100, 20)
+        Me.sciTh.TabIndex = 40
+        Me.sciTh.Text = "0"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(32, 241)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(46, 13)
+        Me.Label7.TabIndex = 39
+        Me.Label7.Text = "Science"
+        '
+        'mathPr
+        '
+        Me.mathPr.Location = New System.Drawing.Point(329, 208)
+        Me.mathPr.Name = "mathPr"
+        Me.mathPr.Size = New System.Drawing.Size(100, 20)
+        Me.mathPr.TabIndex = 38
+        Me.mathPr.Text = "0"
+        '
+        'mathTh
+        '
+        Me.mathTh.Location = New System.Drawing.Point(175, 208)
+        Me.mathTh.Name = "mathTh"
+        Me.mathTh.Size = New System.Drawing.Size(100, 20)
+        Me.mathTh.TabIndex = 37
+        Me.mathTh.Text = "0"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(32, 211)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(36, 13)
+        Me.Label8.TabIndex = 36
+        Me.Label8.Text = "Maths"
+        '
+        'heaPr
+        '
+        Me.heaPr.Location = New System.Drawing.Point(329, 365)
+        Me.heaPr.Name = "heaPr"
+        Me.heaPr.Size = New System.Drawing.Size(100, 20)
+        Me.heaPr.TabIndex = 53
+        Me.heaPr.Text = "0"
+        '
+        'heaTh
+        '
+        Me.heaTh.Location = New System.Drawing.Point(175, 365)
+        Me.heaTh.Name = "heaTh"
+        Me.heaTh.Size = New System.Drawing.Size(100, 20)
+        Me.heaTh.TabIndex = 52
+        Me.heaTh.Text = "0"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(32, 368)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(38, 13)
+        Me.Label9.TabIndex = 51
+        Me.Label9.Text = "Health"
+        '
+        'compPr
+        '
+        Me.compPr.Location = New System.Drawing.Point(329, 335)
+        Me.compPr.Name = "compPr"
+        Me.compPr.Size = New System.Drawing.Size(100, 20)
+        Me.compPr.TabIndex = 50
+        Me.compPr.Text = "0"
+        '
+        'compTh
+        '
+        Me.compTh.Location = New System.Drawing.Point(175, 335)
+        Me.compTh.Name = "compTh"
+        Me.compTh.Size = New System.Drawing.Size(100, 20)
+        Me.compTh.TabIndex = 49
+        Me.compTh.Text = "0"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(32, 338)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(52, 13)
+        Me.Label10.TabIndex = 48
+        Me.Label10.Text = "Computer"
+        '
+        'obtPr
+        '
+        Me.obtPr.Location = New System.Drawing.Point(329, 303)
+        Me.obtPr.Name = "obtPr"
+        Me.obtPr.Size = New System.Drawing.Size(100, 20)
+        Me.obtPr.TabIndex = 47
+        Me.obtPr.Text = "0"
+        '
+        'obtTh
+        '
+        Me.obtTh.Location = New System.Drawing.Point(175, 303)
+        Me.obtTh.Name = "obtTh"
+        Me.obtTh.Size = New System.Drawing.Size(100, 20)
+        Me.obtTh.TabIndex = 46
+        Me.obtTh.Text = "0"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(32, 306)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(29, 13)
+        Me.Label11.TabIndex = 45
+        Me.Label11.Text = "OBT"
+        '
+        'socPr
+        '
+        Me.socPr.Location = New System.Drawing.Point(329, 273)
+        Me.socPr.Name = "socPr"
+        Me.socPr.Size = New System.Drawing.Size(100, 20)
+        Me.socPr.TabIndex = 44
+        Me.socPr.Text = "0"
+        '
+        'socTh
+        '
+        Me.socTh.Location = New System.Drawing.Point(175, 273)
+        Me.socTh.Name = "socTh"
+        Me.socTh.Size = New System.Drawing.Size(100, 20)
+        Me.socTh.TabIndex = 43
+        Me.socTh.Text = "0"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(32, 276)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(36, 13)
+        Me.Label12.TabIndex = 42
+        Me.Label12.Text = "Social"
+        '
+        'morPr
+        '
+        Me.morPr.Location = New System.Drawing.Point(329, 400)
+        Me.morPr.Name = "morPr"
+        Me.morPr.Size = New System.Drawing.Size(100, 20)
+        Me.morPr.TabIndex = 56
+        Me.morPr.Text = "0"
+        '
+        'morTh
+        '
+        Me.morTh.Location = New System.Drawing.Point(175, 400)
+        Me.morTh.Name = "morTh"
+        Me.morTh.Size = New System.Drawing.Size(100, 20)
+        Me.morTh.TabIndex = 55
+        Me.morTh.Text = "0"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(32, 403)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(33, 13)
+        Me.Label13.TabIndex = 54
+        Me.Label13.Text = "Moral"
+        '
+        'saveBtn
+        '
+        Me.saveBtn.Location = New System.Drawing.Point(492, 404)
+        Me.saveBtn.Name = "saveBtn"
+        Me.saveBtn.Size = New System.Drawing.Size(75, 23)
+        Me.saveBtn.TabIndex = 57
+        Me.saveBtn.Text = "Save"
+        Me.saveBtn.UseVisualStyleBackColor = True
+        '
+        'cancelBtn
+        '
+        Me.cancelBtn.Location = New System.Drawing.Point(573, 404)
+        Me.cancelBtn.Name = "cancelBtn"
+        Me.cancelBtn.Size = New System.Drawing.Size(75, 23)
+        Me.cancelBtn.TabIndex = 58
+        Me.cancelBtn.Text = "Cancel"
+        Me.cancelBtn.UseVisualStyleBackColor = True
+        '
+        'termCombo
+        '
+        Me.termCombo.FormattingEnabled = True
+        Me.termCombo.Items.AddRange(New Object() {"First", "Second", "Third", "SendUp"})
+        Me.termCombo.Location = New System.Drawing.Point(122, 36)
+        Me.termCombo.Name = "termCombo"
+        Me.termCombo.Size = New System.Drawing.Size(121, 21)
+        Me.termCombo.TabIndex = 30
+        Me.termCombo.Text = "Choose term..."
+        '
+        'termLabel
+        '
+        Me.termLabel.AutoSize = True
+        Me.termLabel.Location = New System.Drawing.Point(32, 44)
+        Me.termLabel.Name = "termLabel"
+        Me.termLabel.Size = New System.Drawing.Size(31, 13)
+        Me.termLabel.TabIndex = 60
+        Me.termLabel.Text = "Term"
+        '
+        'studentName
+        '
+        Me.studentName.Enabled = False
+        Me.studentName.Location = New System.Drawing.Point(375, 62)
+        Me.studentName.Name = "studentName"
+        Me.studentName.Size = New System.Drawing.Size(170, 20)
+        Me.studentName.TabIndex = 29
+        '
+        'studentRegCombo
+        '
+        Me.studentRegCombo.FormattingEnabled = True
+        Me.studentRegCombo.Location = New System.Drawing.Point(122, 66)
+        Me.studentRegCombo.Name = "studentRegCombo"
+        Me.studentRegCombo.Size = New System.Drawing.Size(121, 21)
+        Me.studentRegCombo.TabIndex = 61
+        Me.studentRegCombo.Text = "Choose reg number..."
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(278, 69)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(75, 13)
+        Me.Label14.TabIndex = 62
+        Me.Label14.Text = "Student Name"
+        '
+        'addResultLowSecForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(670, 439)
+        Me.Controls.Add(Me.studentRegCombo)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.studentName)
+        Me.Controls.Add(Me.termCombo)
+        Me.Controls.Add(Me.termLabel)
+        Me.Controls.Add(Me.cancelBtn)
+        Me.Controls.Add(Me.saveBtn)
+        Me.Controls.Add(Me.morPr)
+        Me.Controls.Add(Me.morTh)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.heaPr)
+        Me.Controls.Add(Me.heaTh)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.compPr)
+        Me.Controls.Add(Me.compTh)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.obtPr)
+        Me.Controls.Add(Me.obtTh)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.socPr)
+        Me.Controls.Add(Me.socTh)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.sciPr)
+        Me.Controls.Add(Me.sciTh)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.mathPr)
+        Me.Controls.Add(Me.mathTh)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.nepPr)
+        Me.Controls.Add(Me.nepTh)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.engPr)
+        Me.Controls.Add(Me.engTh)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.studentRegLabel)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "addResultLowSecForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "addResultLowSecForm"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents studentRegLabel As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents engTh As TextBox
+    Friend WithEvents engPr As TextBox
+    Friend WithEvents nepPr As TextBox
+    Friend WithEvents nepTh As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents sciPr As TextBox
+    Friend WithEvents sciTh As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents mathPr As TextBox
+    Friend WithEvents mathTh As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents heaPr As TextBox
+    Friend WithEvents heaTh As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents compPr As TextBox
+    Friend WithEvents compTh As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents obtPr As TextBox
+    Friend WithEvents obtTh As TextBox
+    Friend WithEvents Label11 As Label
+    Friend WithEvents socPr As TextBox
+    Friend WithEvents socTh As TextBox
+    Friend WithEvents Label12 As Label
+    Friend WithEvents morPr As TextBox
+    Friend WithEvents morTh As TextBox
+    Friend WithEvents Label13 As Label
+    Friend WithEvents saveBtn As Button
+    Friend WithEvents cancelBtn As Button
+    Friend WithEvents termCombo As ComboBox
+    Friend WithEvents termLabel As Label
+    Friend WithEvents studentName As TextBox
+    Friend WithEvents studentRegCombo As ComboBox
+    Friend WithEvents Label14 As Label
+End Class
---
+++ ASMAS/addResultLowSecForm.vb
@@ -0,0 +1,175 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+Public Class addResultLowSecForm
+    Dim Con As System.Data.OleDb.OleDbConnection
+    Private pwd As String
+    Private data_source_path As String = "C:\Users\amWRit\Documents\Visual Studio 2015\Projects\ASMAS\ASMAS\Terse.accdb"
+
+    Public contents As String() = {}
+
+    Public Sub New(params As String())
+        MyBase.New
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        contents = params
+        loadRegNumber(params)
+    End Sub
+
+
+    Private Sub studentRegCombo_SelectedIndexChanged(sender As Object, e As EventArgs) Handles studentRegCombo.SelectedIndexChanged
+        Con = New OleDbConnection
+        Con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & data_source_path & " ;Jet OLEDB:Database Password= & mypassword"
+
+
+        Dim DS = New DataSet
+        Dim SQL As String = ""
+        Try
+            SQL = "SELECT f_name, m_name, l_name, id from student where reg_number = '" & studentRegCombo.Text & "'"
+
+            Con.Open() 'Open connection
+
+            Dim oData As OleDbDataAdapter
+            oData = New OleDbDataAdapter(SQL, Con)
+            Con.Close()
+            oData.Fill(DS)
+
+            Dim Val = DS.Tables(0).Rows(0)(0).ToString & " " & DS.Tables(0).Rows(0)(1).ToString & " " & DS.Tables(0).Rows(0)(2).ToString
+
+            studentName.Text = Val
+
+            ReDim Preserve contents(4)
+            contents(4) = DS.Tables(0).Rows(0)(3).ToString
+        Catch ex As Exception
+            MsgBox(ex.Message)
+
+        Finally
+            'This code gets called regardless of there being errors
+            'This ensures that you close the Database and avoid corrupted data
+            Con.Close()
+        End Try
+    End Sub
+
+    Public Sub loadRegNumber(params As String())
+        Con = New OleDbConnection
+        Con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & data_source_path & " ;Jet OLEDB:Database Password= & mypassword"
+
+
+        Dim DS = New DataSet
+        Dim SQL As String = ""
+        Try
+            SQL = "SELECT * from 
+                    student s
+                    INNER JOIN
+                    (SELECT student_id from 
+                    class c
+                    INNER JOIN
+                    class_student cs
+                    on c.class_id = cs.class_id
+                    where c.class_id =" & params(0) & ") tb
+                    on s.id = tb.student_id"
+
+            Con.Open() 'Open connection
+
+            Dim oData As OleDbDataAdapter
+            oData = New OleDbDataAdapter(SQL, Con)
+            Con.Close()
+            oData.Fill(DS)
+
+            Dim rowCount = DS.Tables(0).Rows.Count
+
+            For i As Integer = 0 To rowCount - 1
+                Dim Val = DS.Tables(0).Rows(i)(1).ToString
+                studentRegCombo.Items.Add(Val)
+            Next
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+
+        Finally
+            'This code gets called regardless of there being errors
+            'This ensures that you close the Database and avoid corrupted data
+            Con.Close()
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles saveBtn.Click
+        Dim textBoxes As TextBox() = {engTh, engPr, nepTh, nepPr, mathTh, mathPr, socTh, socPr, sciTh, sciPr, obtTh, obtPr, compTh, compPr, heaTh, heaPr, morTh, heaPr}
+
+        Dim valid As Boolean = checkNumberValidity(textBoxes)
+
+        If valid = False Then
+            MessageBox.Show("You must enter a Number.", "Data Entry Error", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Exit Sub
+        End If
+
+        Con = New OleDbConnection
+        Con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & data_source_path & " ;Jet OLEDB:Database Password= & mypassword"
+
+        Dim terminal = termCombo.Text
+        Dim class_name = contents(3)
+
+        Try
+            Dim insertSQL As String = "INSERT INTO results_" & class_name & " ([student_id], [school_year], [school_name], [terminal], [eng_th], [eng_pr], [eng_total], [nep_th], [nep_pr], [nep_total], [math_th], [math_pr], [math_total], [sci_th], [sci_pr], [sci_total],
+[soc_th], [soc_pr], [soc_total], [obt_th], [obt_pr], [obt_total], [comp_th], [comp_pr], [comp_total], [hea_th], [hea_pr], [hea_total], [mor_th], [mor_pr], [mor_total]) 
+VALUES
+(@student_id, @school_year, @school_name, @terminal, @eng_th, @eng_pr, @eng_total, @nep_th, @nep_pr, @nep_total, @math_th, @math_pr, @math_total, @sci_th, @sci_pr, @sci_total,
+@soc_th, @soc_pr, @soc_total, @obt_th, @obt_pr, @obt_total, @comp_th, @comp_pr, @comp_total, @hea_th, @hea_pr, @hea_total, @mor_th, @mor_pr, @mor_total)"
+            Con.Open()
+            Dim cmd As New OleDbCommand(insertSQL, Con)
+            cmd.Parameters.AddWithValue("@student_id", contents(4))
+            cmd.Parameters.AddWithValue("@school_year", contents(1))
+            cmd.Parameters.AddWithValue("@school_name", contents(2))
+            cmd.Parameters.AddWithValue("@terminal", terminal)
+            cmd.Parameters.AddWithValue("@eng_th", engTh.Text)
+            cmd.Parameters.AddWithValue("@eng_pr", engPr.Text)
+            cmd.Parameters.AddWithValue("@eng_total", CInt(engTh.Text) + CInt(engPr.Text))
+            cmd.Parameters.AddWithValue("@nep_th", nepTh.Text)
+            cmd.Parameters.AddWithValue("@nep_pr", nepPr.Text)
+            cmd.Parameters.AddWithValue("@nep_total", CInt(nepTh.Text) + CInt(nepPr.Text))
+            cmd.Parameters.AddWithValue("@math_th", mathTh.Text)
+            cmd.Parameters.AddWithValue("@math_pr", mathPr.Text)
+            cmd.Parameters.AddWithValue("@math_total", CInt(mathTh.Text) + CInt(mathPr.Text))
+            cmd.Parameters.AddWithValue("@sci_th", sciTh.Text)
+            cmd.Parameters.AddWithValue("@sci_pr", sciPr.Text)
+            cmd.Parameters.AddWithValue("@sci_total", CInt(sciTh.Text) + CInt(sciPr.Text))
+            cmd.Parameters.AddWithValue("@soc_th", socTh.Text)
+            cmd.Parameters.AddWithValue("@soc_pr", socPr.Text)
+            cmd.Parameters.AddWithValue("@soc_total", CInt(socTh.Text) + CInt(socPr.Text))
+            cmd.Parameters.AddWithValue("@obt_th", obtTh.Text)
+            cmd.Parameters.AddWithValue("@obt_pr", obtPr.Text)
+            cmd.Parameters.AddWithValue("@obt_total", CInt(obtTh.Text) + CInt(obtPr.Text))
+            cmd.Parameters.AddWithValue("@comp_th", compTh.Text)
+            cmd.Parameters.AddWithValue("@comp_pr", compPr.Text)
+            cmd.Parameters.AddWithValue("@comp_total", CInt(compTh.Text) + CInt(compPr.Text))
+            cmd.Parameters.AddWithValue("@hea_th", heaTh.Text)
+            cmd.Parameters.AddWithValue("@hea_pr", heaPr.Text)
+            cmd.Parameters.AddWithValue("@hea_total", CInt(heaTh.Text) + CInt(heaPr.Text))
+            cmd.Parameters.AddWithValue("@mor_th", morTh.Text)
+            cmd.Parameters.AddWithValue("@mor_pr", morPr.Text)
+            cmd.Parameters.AddWithValue("@mor_total", CInt(morTh.Text) + CInt(morPr.Text))
+
+
+
+            cmd.ExecuteNonQuery()
+
+            MsgBox("Insert Successful", MsgBoxStyle.Information, "INSERTED")
+            Con.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            Con.Close()
+            'AddTable()
+        End Try
+    End Sub
+
+    Public Function checkNumberValidity(textboxes As TextBox()) As Boolean
+        For i As Integer = 0 To textboxes.Count - 1
+            If Not IsNumeric(textboxes(i).Text) Or textboxes(i).Text = "" Then
+                Return False
+            End If
+        Next
+        Return True
+    End Function
+End Class
