f9d3087f411c3205b100adec72cc669a9d41e7f7
amWRit <derasrocks@gmail.com>
2016-09-11 15:08:49 +0545

    moved file

---
+++ ASMAS/Result/classResults.Designer.vb
@@ -0,0 +1,188 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class classResults
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.classResultListView = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.titleLabel = New System.Windows.Forms.Label()
+        Me.termCombo = New System.Windows.Forms.ComboBox()
+        Me.termLabel = New System.Windows.Forms.Label()
+        Me.viewBtn = New System.Windows.Forms.Button()
+        Me.updateRankBtn = New System.Windows.Forms.Button()
+        Me.classResultsContextMenu = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.printBtn = New System.Windows.Forms.Button()
+        Me.updateCalcBtn = New System.Windows.Forms.Button()
+        Me.PrintToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.classResultsContextMenu.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'classResultListView
+        '
+        Me.classResultListView.AllowColumnReorder = True
+        Me.classResultListView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.classResultListView.CausesValidation = False
+        Me.classResultListView.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2})
+        Me.classResultListView.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.classResultListView.FullRowSelect = True
+        Me.classResultListView.GridLines = True
+        Me.classResultListView.Location = New System.Drawing.Point(0, 57)
+        Me.classResultListView.MultiSelect = False
+        Me.classResultListView.Name = "classResultListView"
+        Me.classResultListView.Size = New System.Drawing.Size(1008, 672)
+        Me.classResultListView.TabIndex = 166
+        Me.classResultListView.UseCompatibleStateImageBehavior = False
+        Me.classResultListView.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Width = 112
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Width = 294
+        '
+        'titleLabel
+        '
+        Me.titleLabel.AutoSize = True
+        Me.titleLabel.Font = New System.Drawing.Font("Nexa Light", 9.749999!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.titleLabel.Location = New System.Drawing.Point(12, 9)
+        Me.titleLabel.Name = "titleLabel"
+        Me.titleLabel.Size = New System.Drawing.Size(89, 16)
+        Me.titleLabel.TabIndex = 167
+        Me.titleLabel.Text = "Class results :"
+        '
+        'termCombo
+        '
+        Me.termCombo.FormattingEnabled = True
+        Me.termCombo.Items.AddRange(New Object() {"First", "Second", "Third", "SendUp"})
+        Me.termCombo.Location = New System.Drawing.Point(375, 9)
+        Me.termCombo.Name = "termCombo"
+        Me.termCombo.Size = New System.Drawing.Size(121, 21)
+        Me.termCombo.TabIndex = 1
+        Me.termCombo.Text = "Choose term..."
+        '
+        'termLabel
+        '
+        Me.termLabel.AutoSize = True
+        Me.termLabel.Location = New System.Drawing.Point(337, 14)
+        Me.termLabel.Name = "termLabel"
+        Me.termLabel.Size = New System.Drawing.Size(31, 13)
+        Me.termLabel.TabIndex = 172
+        Me.termLabel.Text = "Term"
+        '
+        'viewBtn
+        '
+        Me.viewBtn.Location = New System.Drawing.Point(502, 9)
+        Me.viewBtn.Name = "viewBtn"
+        Me.viewBtn.Size = New System.Drawing.Size(75, 23)
+        Me.viewBtn.TabIndex = 2
+        Me.viewBtn.Text = "View"
+        Me.viewBtn.UseVisualStyleBackColor = True
+        '
+        'updateRankBtn
+        '
+        Me.updateRankBtn.Location = New System.Drawing.Point(884, 9)
+        Me.updateRankBtn.Name = "updateRankBtn"
+        Me.updateRankBtn.Size = New System.Drawing.Size(99, 23)
+        Me.updateRankBtn.TabIndex = 5
+        Me.updateRankBtn.Text = "Update Rank"
+        Me.updateRankBtn.UseVisualStyleBackColor = True
+        '
+        'classResultsContextMenu
+        '
+        Me.classResultsContextMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EditToolStripMenuItem, Me.PrintToolStripMenuItem})
+        Me.classResultsContextMenu.Name = "searchResultContextMenu"
+        Me.classResultsContextMenu.Size = New System.Drawing.Size(153, 70)
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.EditToolStripMenuItem.Text = "Edit"
+        '
+        'printBtn
+        '
+        Me.printBtn.Enabled = False
+        Me.printBtn.Location = New System.Drawing.Point(764, 9)
+        Me.printBtn.Name = "printBtn"
+        Me.printBtn.Size = New System.Drawing.Size(101, 23)
+        Me.printBtn.TabIndex = 4
+        Me.printBtn.Text = "Print Report Card"
+        Me.printBtn.UseVisualStyleBackColor = True
+        '
+        'updateCalcBtn
+        '
+        Me.updateCalcBtn.Location = New System.Drawing.Point(630, 8)
+        Me.updateCalcBtn.Name = "updateCalcBtn"
+        Me.updateCalcBtn.Size = New System.Drawing.Size(114, 23)
+        Me.updateCalcBtn.TabIndex = 3
+        Me.updateCalcBtn.Text = "Update Calculations"
+        Me.updateCalcBtn.UseVisualStyleBackColor = True
+        '
+        'PrintToolStripMenuItem
+        '
+        Me.PrintToolStripMenuItem.Name = "PrintToolStripMenuItem"
+        Me.PrintToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.PrintToolStripMenuItem.Text = "Print"
+        '
+        'classResults
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1008, 729)
+        Me.Controls.Add(Me.updateCalcBtn)
+        Me.Controls.Add(Me.printBtn)
+        Me.Controls.Add(Me.updateRankBtn)
+        Me.Controls.Add(Me.termCombo)
+        Me.Controls.Add(Me.termLabel)
+        Me.Controls.Add(Me.viewBtn)
+        Me.Controls.Add(Me.titleLabel)
+        Me.Controls.Add(Me.classResultListView)
+        Me.MaximizeBox = False
+        Me.Name = "classResults"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Class Results"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.classResultsContextMenu.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents classResultListView As ListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents titleLabel As Label
+    Friend WithEvents termCombo As ComboBox
+    Friend WithEvents termLabel As Label
+    Friend WithEvents viewBtn As Button
+    Friend WithEvents updateRankBtn As Button
+    Friend WithEvents classResultsContextMenu As ContextMenuStrip
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents printBtn As Button
+    Friend WithEvents updateCalcBtn As Button
+    Friend WithEvents PrintToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ ASMAS/Result/classResults.vb
@@ -0,0 +1,214 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+Public Class classResults
+    Dim Con As System.Data.OleDb.OleDbConnection
+    Private pwd As String
+    Private data_source_path As String = DBConnection.data_source_path
+
+    Public contents As String()
+    Public tempDS As DataSet
+
+    '{itemID, yearnum, schoolname, classname}
+    Public Sub New(ByVal params As String())
+        MyBase.New
+        ' This call is required by the designer.
+        InitializeComponent()
+        contents = params
+        Dim classID = params(0)
+        refreshLV(classID, "")
+
+    End Sub
+
+
+    Private Sub viewBtn_Click(sender As Object, e As EventArgs) Handles viewBtn.Click
+        printBtn.Enabled = False
+        Dim terminal = termCombo.Text
+        refreshLV(contents(0), terminal)
+    End Sub
+
+    Public Sub refreshLV(classID As String, terminal As String)
+        Con = New OleDbConnection
+        Con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & data_source_path & " ;Jet OLEDB:Database Password= & mypassword"
+
+        Dim className = contents(3)
+        Dim DS = New DataSet
+        Dim SQL As String = ""
+        Dim classSQL As String = "Select distinct short_name from class where class_id=" & classID
+        Dim oData As OleDbDataAdapter
+
+        titleLabel.Text = "Class Results: " & className & " - " & terminal
+
+        Try
+            SQL = "SELECT * from results_" & className & " where school_name='" & contents(2) & "' and school_year ='" & contents(1) & "'"
+            If terminal <> "" Then
+                SQL = SQL & " and terminal = '" & terminal & "'"
+            End If
+
+            DS.Tables.Clear()
+            Con.Open() 'Open connection
+
+            oData = New OleDbDataAdapter(SQL, Con)
+            Con.Close()
+            oData.Fill(DS)
+            tempDS = DS
+
+            classResultListView.Items.Clear() 'prep Listview by clearing it
+            classResultListView.Columns.Clear() 'remove columns in LV
+
+            'create columns on listview
+            For i As Integer = 0 To DS.Tables(0).Columns.Count - 1
+                classResultListView.Columns.Add(DS.Tables(0).Columns(i).Caption, 100, HorizontalAlignment.Left)
+            Next
+
+            'Parse and add data to the listview
+            For i As Integer = 0 To DS.Tables(0).Rows.Count - 1
+                Dim xItem As New ListViewItem(DS.Tables(0).Rows(i)(0).ToString)
+                For j As Integer = 1 To DS.Tables(0).Columns.Count - 1
+                    xItem.SubItems.Add(DS.Tables(0).Rows(i)(j).ToString)
+                Next
+                classResultListView.Items.Add(xItem)
+            Next
+
+            If DS.Tables(0).Rows.Count > 0 Then printBtn.Enabled = True
+        Catch ex As Exception
+            MsgBox(ex.Message)
+
+        Finally
+            'This code gets called regardless of there being errors
+            'This ensures that you close the Database and avoid corrupted data
+            Con.Close()
+        End Try
+    End Sub
+
+    Private Sub updateRankBtn_Click(sender As Object, e As EventArgs) Handles updateRankBtn.Click
+        Con = New OleDbConnection
+        Con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & data_source_path & " ;Jet OLEDB:Database Password= & mypassword"
+
+        If termCombo.SelectedIndex = -1 Then
+            MessageBox.Show("Please select a terminal to update rank.", "Incomplete input.", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        Else
+
+            Dim DS = New DataSet
+            Dim SQL As String = ""
+            Dim oData As OleDbDataAdapter
+
+            Dim terminal = termCombo.Text
+            Dim className = contents(3)
+            Dim rankSQL As String = "SELECT id, student_id, percentage from results_" & className & " where school_name='" & contents(2) & "' and school_year ='" & contents(1) &
+            "' and terminal = '" & terminal & "' order by percentage DESC"
+
+            Try
+                Con.Open()
+                oData = New OleDbDataAdapter(rankSQL, Con)
+                Con.Close()
+                oData.Fill(DS)
+
+                Dim rowCount = DS.Tables(0).Rows.Count
+                Dim insertSQL = ""
+                Dim rowID As Integer
+                For i As Integer = 0 To rowCount - 1
+                    rowID = CInt(DS.Tables(0).Rows(i)(0))
+                    insertSQL = "UPDATE results_" & className & " set rank = @rank where id=" & rowID
+                    Con.Open()
+                    Dim cmd As New OleDbCommand(insertSQL, Con)
+
+                    cmd.Parameters.AddWithValue("@rank", i + 1)
+
+                    cmd.ExecuteNonQuery()
+                    Con.Close()
+                Next
+                MsgBox("Rank updated successfully. Please make sure of it.", MsgBoxStyle.Information, "Updated")
+                refreshLV(className, terminal)
+            Catch ex As Exception
+                MsgBox(ex.Message)
+
+            Finally
+                'This code gets called regardless of there being errors
+                'This ensures that you close the Database and avoid corrupted data
+                Con.Close()
+            End Try
+        End If
+    End Sub
+
+    Private Sub classResultListView_MouseUp(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles classResultListView.MouseUp
+        If e.Button = MouseButtons.Right Then
+            If classResultListView.GetItemAt(e.X, e.Y) IsNot Nothing Then
+                classResultListView.GetItemAt(e.X, e.Y).Selected = True
+                classResultsContextMenu.Show(classResultListView, New Point(e.X, e.Y))
+            End If
+        End If
+    End Sub
+
+    Private Sub EditToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EditToolStripMenuItem.Click
+        Dim ItemIndex As Integer = classResultListView.SelectedIndices(0) 'Grab the selected Index
+        Dim studentID = classResultListView.Items(ItemIndex).SubItems(1).Text
+
+        Dim primary As String() = {"1", "2", "3", "4", "5"}
+        Dim lowSec As String() = {"6E", "6N", "7E", "7N", "8E", "8N"}
+        Dim sec As String() = {"9E", "9N", "10E", "10A"}
+
+        Dim className = contents(3)
+        Dim year_num = classResultListView.Items(ItemIndex).SubItems(4).Text
+        Dim school_name = classResultListView.Items(ItemIndex).SubItems(5).Text
+        Dim terminal = classResultListView.Items(ItemIndex).SubItems(6).Text
+        Dim edit = "TRUE"
+
+        Dim params As String() = {studentID, year_num, school_name, className, terminal, edit}
+
+        If primary.Contains(className) Then
+            Dim _newClassResult As New addResultPrimaryForm(params)
+            _newClassResult.Show()
+        ElseIf lowSec.Contains(className) Then
+            Dim _newClassResult As New addResultLowSecForm(params)
+            _newClassResult.Show()
+        ElseIf sec.Contains(className) Then
+            Dim _newClassResult As New addResultSecForm(params)
+            _newClassResult.Show()
+        End If
+    End Sub
+
+    Private Sub printBtn_Click(sender As Object, e As EventArgs) Handles printBtn.Click
+        letsPrint(0) 'index = 0; start from beginning
+    End Sub
+
+    Private Sub letsPrint(index As Integer)
+        Dim primary As String() = {"1", "2", "3", "4", "5"}
+        Dim lowSec As String() = {"6E", "6N", "7E", "7N", "8E", "8N"}
+        Dim sec As String() = {"9E", "9N", "10E", "10A"}
+
+        Dim class_name = contents(3)
+        Dim year_num = contents(1)
+        Dim school_name = contents(2)
+
+        Dim class_teacher = myFunctions.getClassTeacherName(school_name, year_num, class_name)
+
+        If primary.Contains(class_name) Then
+            Dim printForm As New printResultsPrimaryForm(tempDS, index, class_name, class_teacher)
+            printForm.Show()
+        ElseIf lowSec.Contains(class_name) Then
+            Dim printForm As New printResultLowSecForm(tempDS, index, class_name, class_teacher)
+            printForm.Show()
+        ElseIf sec.Contains(class_name) Then
+            Dim printForm As New printResultSecForm(tempDS, index, class_name, class_teacher)
+            printForm.Show()
+        End If
+    End Sub
+
+
+    Private Sub updateCalcBtn_Click(sender As Object, e As EventArgs) Handles updateCalcBtn.Click
+        If termCombo.SelectedIndex = -1 Then
+            MessageBox.Show("Please select a terminal to update calcuations.", "Incomplete input.", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+        Dim class_name = contents(3)
+        Dim year_num = contents(1)
+        Dim school_name = contents(2)
+        resultFunctions.updateCalculations(tempDS, school_name, year_num, class_name)
+        refreshLV(class_name, termCombo.Text)
+    End Sub
+
+    Private Sub PrintToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PrintToolStripMenuItem.Click
+        Dim ItemIndex As Integer = classResultListView.SelectedIndices(0) 'Grab the selected Index
+        letsPrint(ItemIndex)
+    End Sub
+End Class
